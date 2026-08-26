0cabb33c43e1f85ddddb4c26fa2f5b8e41962e0f
Rai Butera <rai@rbutera.com>
2026-08-26 23:58:33 +0100

    test: drop comment that restates the assertion

--- packages/server/src/index.test.ts
+++ packages/server/src/index.test.ts
@@ -4,7 +4,6 @@ import { IMPLEMENTED_PROTOCOL_VERSION } from "./index.js";
 
 describe("@whtbrd/server", () => {
   it("implements the protocol version declared by @whtbrd/core", () => {
-    // Cross-package resolution proof: server consumes core's exported constant.
     expect(IMPLEMENTED_PROTOCOL_VERSION).toBe(PROTOCOL_VERSION);
     expect(IMPLEMENTED_PROTOCOL_VERSION).toBe("0.1");
   });
